4009d3edc8795b1b9d214eeb0899fa8aac0c95f8
ArianRamadan1 <148253454+ArianRamadan1@users.noreply.github.com>
2026-07-08 10:26:02 +0000

    bug fix

--- app.py
+++ app.py
@@ -1,56 +1,71 @@
 import streamlit as st
 from pymongo import MongoClient
 
-# MongoDB Connection
-client = MongoClient("mongodb://localhost:27017/")
-db = client["BMI_DB"]
-collection = db["users"]
+# MongoDB Atlas Connection
+MONGO_URI = "mongodb+srv://username:password@cluster0.xxxxx.mongodb.net/"
 
-st.title("🏋️ Body Mass Index Calculator")
+try:
+    client = MongoClient(MONGO_URI)
+    client.admin.command("ping")
+
+    db = client["BMI_DB"]
+    collection = db["users"]
+
+except Exception as e:
+    st.error(f"MongoDB Connection Error: {e}")
+
+
+st.title("Body Mass Index Calculator")
 
 # Inputet
 name = st.text_input("Emri")
 height_cm = st.text_input("Gjatesia (cm)")
 weight = st.text_input("Pesha (kg)")
 
+
 if st.button("Llogarit BMI"):
     try:
-        # Konvertimi në numra
+        # Konvertimi i inputeve
         height_cm = float(height_cm)
         weight = float(weight)
 
-        # Konverto cm -> m
-        height = height_cm / 100
-
-        # Llogaritja e BMI
-        bmi = weight / (height ** 2)
-
-        # Kategoria
-        if bmi < 18.5:
-            category = "Underweight"
-        elif bmi < 25:
-            category = "Normal"
-        elif bmi < 30:
-            category = "Overweight"
+        # Kontroll
+        if height_cm <= 0 or weight <= 0:
+            st.error("Gjatesia dhe pesha duhet te jene me te medha se 0.")
         else:
-            category = "Obese"
+            # Cm ne meter
+            height = height_cm / 100
 
-        # Shfaq rezultatin
-        st.success(f"BMI: {bmi:.2f}")
-        st.info(f"Kategoria: {category}")
+            # Llogaritja BMI
+            bmi = weight / (height ** 2)
 
-        # Ruaj në MongoDB
-        data = {
-            "name": name,
-            "height_cm": height_cm,
-            "weight": weight,
-            "BMI": round(bmi, 2),
-            "category": category
-        }
+            # Kategoria
+            if bmi < 18.5:
+                category = "Underweight"
+            elif bmi < 25:
+                category = "Normal"
+            elif bmi < 30:
+                category = "Overweight"
+            else:
+                category = "Obese"
 
-        collection.insert_one(data)
+            # Shfaq rezultatin
+            st.success(f"BMI: {bmi:.2f}")
+            st.info(f"Kategoria: {category}")
 
-        st.success("Te dhenat u ruajten ne MongoDB.")
+            # Ruajtja ne MongoDB
+            data = {
+                "name": name,
+                "height_cm": height_cm,
+                "weight_kg": weight,
+                "BMI": round(bmi, 2),
+                "category": category
+            }
+
+            collection.insert_one(data)
+
+            st.success("Te dhenat u ruajten ne MongoDB.")
+
 
     except ValueError:
         st.error("Ju lutem shkruani vlera numerike te sakta per gjatesine dhe peshen.")
